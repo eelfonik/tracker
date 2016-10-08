c759c69e3d093dae386fd1d25db6356121d535a9
kino lee <kinoflee@gmail.com>
2016-10-08 16:27:46 +0200

    correct typo

--- dist/bundle.js
+++ dist/bundle.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "9348c01f5505b1f26c8f"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "24b08693c59e9c5f6c8c"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
@@ -14708,9 +14708,9 @@
 
 	var _reactDom2 = _interopRequireDefault(_reactDom);
 
-	var _AppRoutes = __webpack_require__(122);
+	var _appRoutes = __webpack_require__(123);
 
-	var _AppRoutes2 = _interopRequireDefault(_AppRoutes);
+	var _appRoutes2 = _interopRequireDefault(_appRoutes);
 
 	var _layout = __webpack_require__(78);
 
@@ -14718,7 +14718,7 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	_reactDom2.default.render(_react2.default.createElement(_AppRoutes2.default, null), document.getElementById('root'));
+	_reactDom2.default.render(_react2.default.createElement(_appRoutes2.default, null), document.getElementById('root'));
 
 	// window.onload = () => {
 	//     ReactDOM.render(<AppRoutes/>, document.getElementById('main'));
@@ -14726,59 +14726,6 @@
 
 /***/ },
 /* 122 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-
-	Object.defineProperty(exports, "__esModule", {
-	    value: true
-	});
-
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-	var _react = __webpack_require__(6);
-
-	var _react2 = _interopRequireDefault(_react);
-
-	var _reactRouter = __webpack_require__(55);
-
-	var _routes = __webpack_require__(124);
-
-	var _routes2 = _interopRequireDefault(_routes);
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var AppRoutes = function (_React$Component) {
-	    _inherits(AppRoutes, _React$Component);
-
-	    function AppRoutes() {
-	        _classCallCheck(this, AppRoutes);
-
-	        return _possibleConstructorReturn(this, (AppRoutes.__proto__ || Object.getPrototypeOf(AppRoutes)).apply(this, arguments));
-	    }
-
-	    _createClass(AppRoutes, [{
-	        key: 'render',
-	        value: function render() {
-	            return _react2.default.createElement(_reactRouter.Router, { history: _reactRouter.browserHistory, routes: _routes2.default, onUpdate: function onUpdate() {
-	                    return window.scrollTo(0, 0);
-	                } });
-	        }
-	    }]);
-
-	    return AppRoutes;
-	}(_react2.default.Component);
-
-	exports.default = AppRoutes;
-
-/***/ },
-/* 123 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -14836,6 +14783,59 @@
 	exports.default = IndexPage;
 
 /***/ },
+/* 123 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(6);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _reactRouter = __webpack_require__(55);
+
+	var _routes = __webpack_require__(124);
+
+	var _routes2 = _interopRequireDefault(_routes);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var AppRoutes = function (_React$Component) {
+	    _inherits(AppRoutes, _React$Component);
+
+	    function AppRoutes() {
+	        _classCallCheck(this, AppRoutes);
+
+	        return _possibleConstructorReturn(this, (AppRoutes.__proto__ || Object.getPrototypeOf(AppRoutes)).apply(this, arguments));
+	    }
+
+	    _createClass(AppRoutes, [{
+	        key: 'render',
+	        value: function render() {
+	            return _react2.default.createElement(_reactRouter.Router, { history: _reactRouter.browserHistory, routes: _routes2.default, onUpdate: function onUpdate() {
+	                    return window.scrollTo(0, 0);
+	                } });
+	        }
+	    }]);
+
+	    return AppRoutes;
+	}(_react2.default.Component);
+
+	exports.default = AppRoutes;
+
+/***/ },
 /* 124 */
 /***/ function(module, exports, __webpack_require__) {
 
@@ -14855,7 +14855,7 @@
 
 	var _layout2 = _interopRequireDefault(_layout);
 
-	var _IndexPage = __webpack_require__(123);
+	var _IndexPage = __webpack_require__(122);
 
 	var _IndexPage2 = _interopRequireDefault(_IndexPage);
 
--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRoutes from './components/AppRoutes';
+import AppRoutes from './components/appRoutes';
 
 // window.onload = () => {
 //     ReactDOM.render(<AppRoutes/>, document.getElementById('main'));
