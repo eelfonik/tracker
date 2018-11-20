45dd7dcb696ceabb0da8e07afe3a206e2fd8e2b0
kino <kinoflee@gmail.com>
2018-11-20 18:47:34 +0100

    adding experiment for changing chart type by select

--- src/components/InvoicesTrends.tsx
+++ src/components/InvoicesTrends.tsx
@@ -1,17 +1,28 @@
 import * as React from 'react'
 import {InvoiceInfo} from '../store/types'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, BarChart, Bar } from 'recharts';
 // import * as R from 'ramda'
 
-function InvoicesTrends(props: {userInvoices: Array<InvoiceInfo>}) {
+function InvoicesTrends(props: {userInvoices: Array<InvoiceInfo>, chartType: string}) {
   const sums = props.userInvoices.map(invoice => ({date: invoice.date, sum: invoice.sum}))
-  return (
+  return props.chartType === 'line' ? (
     <LineChart width={600} height={300} data={sums}>
       <Line type="monotone" dataKey="sum" stroke="#8884d8" />
       <CartesianGrid stroke="#ccc" />
       <XAxis dataKey="date" />
       <YAxis />
+      <Tooltip />
+      <Legend />
     </LineChart>
+  ) : (
+    <BarChart width={730} height={250} data={sums}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="date" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="sum" fill="#82ca9d" />
+    </BarChart>
   )
 }
 
